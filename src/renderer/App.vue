80dfb442f0b875e3bcae09924851910082c4f9f5
Franky So <frankyso.mail@gmail.com>
2018-04-19 11:52:09 +0700

    missing file

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'kiosbayar'
+  }
+</script>
+
+<style>
+  @import "~bootstrap/dist/css/bootstrap.css";
+  @import "~bootstrap-vue/dist/bootstrap-vue.css";
+  /* CSS */
+</style>
